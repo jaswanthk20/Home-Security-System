f43a43d019392c5cf379834fa9b5e7f41ea14233
NH39 <126912528+NH39@users.noreply.github.com>
2023-03-11 12:37:00 -0400

    correct lcd code

--- LCD_SCREEN.c
+++ LCD_SCREEN.c
@@ -1,76 +1,192 @@
 /*
- * LCD SCREEN.c
- *
- * Created: 2023-03-05 8:21:26 PM
+ * 
  * Author : nader
+ *
+ * Code to drive the LCD screen
+ * Fill in the missing code everywhere there is an "xx"
  */ 
 
-
-#define epin = PINC0
-#define eport = PORTC
-#define rspin = PINB5
-#define rsport = PORTB
-#define d1 = PINB4
-#define d2 = PIND4
-#define d3 = PIND3
-#define d4 = PIND2
-#define data = 
-
 #include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
 
 
-void screen_cmd(unsigned char ch);
+#define RS PORTC2
+#define RW PORTC3
+#define CE PORTC4
+
+#define DB4 PORTD4
+#define DB5 PORTD5
+#define DB6 PORTD6
+#define DB7 PORTD7
+
+void LCD_init (void);
+void LCD_command (char command);
+void LCD_command_4bit (char command);
+void LCD_Char (char AsciiChar);
+void LCD_Send_A_String(char *StringOfCharacters);
+void LCD_clearScreen (void);
+void LCD_home(void);
+void LCD_display(void);
+void LCD_noDisplay(void);
 
 int main(void)
 {
-	screen_cmd(0x28);
-	screen_cmd(0x0e);
-    DDRB = 0xff;
-	DDRD = 0xff;
-	DDRC = 0xff;	
 	
-	/* Replace with your application code */
-    while (1) 
-    {
-		
-    }
+	DDRC |= (1<<RW)|(1<<CE)|(1<<RS); //Set the display control pins as outputs (PC2, PC3, PC4)
+
+	DDRD |= (1<<DB4)|(1<<DB5)|(1<<DB6)|(1<<DB7); //Set the parallel data pins as outputs (PD4, PD5, PD6, PD7)
+	
+	//set data pin outputs to low to start
+	PORTC = ~(1<<RS) & ~(1<<RW) & ~(1<<CE);
+
+	LCD_init();
+	
+	LCD_command(0x0E); //Display on, cursor on (0x0C for display ON, cursor Off)
+	LCD_command(0x1C); //Increment address by 1 and shift the cursor to the right at the time of write
+
+	LCD_Send_A_String("Hello World!"); //send "Hello World!" to the LCD
+  	_delay_ms(5000);
+	while (1)
+	{
+	};
 }
 
-void screen_cmd(int select, char *data_in){
-	char data;
+void LCD_init(void)
+{
+	_delay_ms(40); //LCD power on delay, needs to be greater than 15ms
 	
-	for(int i = 0 ; data_in[i] != '\0'; i++);
+	//Manual 4 bit initialization of LCD, not likely required, but doesn't harm to do it
+	LCD_command_4bit(0x3);
+	_delay_ms(5); //min 4.1ms
+	LCD_command_4bit(0x3);
+	_delay_ms(1);
+	LCD_command_4bit(0x3);
+	_delay_ms(1); //min of 100us
 	
-	int length = i;
+	//DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0    <== See table in Datasheet, * indicated not used
+	//0    0   1   0  0*   0*   0*  0*
 	
-	for(int j = 0; j < i; j++){
-		if(j==16){
-			screen_cmd(0, 0xC0);
-		}
-		
-		data = data_in[j];
-		if(select == 1){
-			rsport = (1<<rspin);
-		}else if(select == 0){
-			rsport &= ~(1<<rspin);
-		}
-		
-		
+	LCD_command_4bit(0x2); //Function set to 4 bit
+
+	LCD_command(0x28); //2 line, 5*8 dots, 4 bit mode
+	LCD_command(0x08); //Display off, cursor off (0x0C for display ON, cursor Off)
+	LCD_command(0x01); //Display clear
+	LCD_command(0x06); //Entry Set mode, increment cursor, no shift
+	
+	_delay_ms(2);
+}
+void LCD_command (char command)
+{
+	//Basic function used in giving commands to the LCD
+	char UpperHalf, LowerHalf;
+	
+	UpperHalf=command & 0xF0;	//Take upper 4 bits of command
+	PORTD &= 0x0F; //Flushes upper half of PortC to 0, but keeps lower half
+	PORTD |= UpperHalf;
+	PORTC &=~(1<<RS); //Clear RS for command register
+	//PORTC &=~(1<<RW); //Clear RW for IR
+	PORTC |= (1<<CE); //Set CE
+	_delay_us(1);
+	PORTC &= ~(1<<CE); //Clear CE
+	_delay_us(200);
+	
+	LowerHalf=(command<<4); //Lower 4 bits of the command
+	PORTD &= 0x0F; //Flushes upper half of PortC to 0, but keeps lower half
+	PORTD |= LowerHalf;
+	PORTC |= (1<<CE); //Set CE
+	_delay_us(1);
+	PORTC &= ~(1<<CE); //clear CE
+	_delay_ms(2);
+	
+}
+
+void LCD_command_4bit (char command)
+{
+	//Basic function used in giving commands to the LCD
+	char LowerHalf;
+	
+	LowerHalf=(command<<4); //Lower 4 bits of the command
+	PORTD &= 0x0F; //Flushes upper half of PortC to 0, but keeps lower half
+	PORTD |= LowerHalf;
+	PORTC &=~(1<<RS); //Clear RS for command register
+	PORTC |= (1<<CE); //Set CE
+	_delay_us(1);
+	PORTC &= ~(1<<CE); //clear CE
+	_delay_ms(2);
+	
+}
+
+void LCD_Char (char AsciiChar)
+{
+	char UpperHalf, LowerHalf;
+	
+	UpperHalf=AsciiChar & 0xF0; //Upper 4 bits of data
+	PORTD &= 0x0F; //Flushes upper half of PORTD to 0, but keeps lower half
+	PORTD |= UpperHalf;
+	PORTC |=(1<<RS); //Set RS for data register
+	//PORTC &=~(1<<RW); //Clear RW for write data, Set RW to read data
+	PORTC |= (1<<CE); //Set CE
+	_delay_us(1);
+	PORTC &= ~(1<<CE); //Clear CE
+	_delay_us(200);
+	
+	LowerHalf=(AsciiChar<<4); //Lower 4 bits of the command
+	PORTD &= 0x0F; //Flushes upper half of PORTD to 0, but keeps lower half
+	PORTD |= LowerHalf;
+	PORTC |= (1<<CE); //Set CE
+	_delay_us(1);
+	PORTC &= ~(1<<CE);
+	_delay_ms(2);
+	
+}
+void LCD_Send_A_String(char *StringOfCharacters)
+{
+	//Take a string input and displays it
+	//Each character in the string is processed using LCD_Char which converts the character into the proper 8bit hex #
+	//Max character in a string should be <16, after 16th character, everything will be ignored
+
+	int i;
+	for (i=0;StringOfCharacters[i]!=0;i++) //Send each character of string until the Null
+	{
+		LCD_Char(StringOfCharacters[i]);
 	}
-	
-	
-	data = ch;
-	rspin = 0;
-	epin = 1;
-	
-	//delay 10 ms
-	
-	epin = 0;
-	
+		
 }
 
-void screen_data(unsigned char ch){
+void LCD_clearScreen (void)
+{
+	//Clears the screen 
+	//DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
+	//0    0   0   0  0    0   0  1
+	LCD_command(0X01); //Clear display
+	_delay_ms(2);
 	
+	//Returns the cursor to (0,0) position
+	//DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
+	//0    0   0   0  1    0   0  0	
+	LCD_command(0X08); //cursor at home position
 }
 
+void LCD_home(void)
+{
+	//Returns the cursor to (0,0) position
+	//DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
+	//0    0   0   0  0    0   1  0	
+	LCD_command(0x02); 
+}
 
+void LCD_display(void)
+{
+	//Display ON with cursor OFF
+	//DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
+	//0    0   0   0  1    1   0  0
+	LCD_command(0x0C);
+}
+void LCD_noDisplay(void)
+{
+	//Display off
+	//DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
+	//0    0   0   0  1    0   0  0
+	LCD_command(0x08);
+}
